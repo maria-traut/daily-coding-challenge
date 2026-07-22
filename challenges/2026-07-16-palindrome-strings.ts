f7524bc07b48c201601b57322b70974ae94bf9f7
Maria Traut <mia.gotcha@proton.me>
2026-07-22 11:38:04 +0200

    install bun and add challenges

---
+++ challenges/2026-07-16-palindrome-strings.ts
@@ -0,0 +1,18 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward
+or forward. This includes capital letters, punctuation, and word dividers.
+Implement a function that checks if something is a palindrome.
+*/
+
+import { test } from "@/test.ts";
+
+function isPalindrome(line: string): boolean {
+  const splitLine = line.split("").reverse().join("");
+  return splitLine === line;
+}
+
+test(isPalindrome("racecar"), true);
+test(isPalindrome("hello"), false);
+test(isPalindrome("a"), true);
+test(isPalindrome(""), true);
+test(isPalindrome("madam"), true);
